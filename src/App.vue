df937cad6bc3132679713617f1d6ba3a3e7f4fa4
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2023-08-28 12:18:38 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue World",
+    };
+  },
+};
+</script>
+
+<!-- html -->
+<template>
+  <div class="p-4" id="app">
+    <div class="flex justify-center items-center ml-0 mr-0 mt-auto mb-auto">
+      <div class="rounded-xl p-10 bg-blue-500 text-white text-center text-2xl">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        message: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div class="bg-blue-500 p-4 rounded-lg">
+      <h1 class="text-center text-white font-sans text-2xl">
+        {{ message }}
+      </h1>
+    </div>
+</template>
